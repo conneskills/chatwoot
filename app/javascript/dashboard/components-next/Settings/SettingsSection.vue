51df1f38ddb9da4ea8a4cda3b205cf1426ce10a2
iamsivin <iamsivin@gmail.com>
2026-01-29 18:07:36 +0530

    chore: Update pre-chat-form settings section

---
+++ app/javascript/dashboard/components-next/Settings/SettingsSection.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ToggleSwitch from 'dashboard/components-next/switch/Switch.vue';
+
+defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+});
+
+const modelValue = defineModel({ type: Boolean, default: false });
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-start outline outline-1 -outline-offset-1 outline-n-weak rounded-xl"
+  >
+    <div class="flex flex-col gap-1 items-start w-full px-4 py-3">
+      <div class="flex items-center gap-3 w-full justify-between">
+        <span class="text-heading-3 text-n-slate-12">
+          {{ header }}
+        </span>
+        <ToggleSwitch v-model="modelValue" />
+      </div>
+      <span v-if="description" class="text-body-main text-n-slate-11">
+        {{ description }}
+      </span>
+    </div>
+    <div
+      v-if="$slots.editor"
+      class="px-4 pb-4 pt-2 w-full border-t border-n-weak"
+    >
+      <slot name="editor" />
+    </div>
+  </div>
+</template>
