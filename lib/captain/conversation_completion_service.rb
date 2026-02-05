894e276b316f4c7e4e5a0a86e6858fce3703fa89
aakashb95 <aakashbakhle@gmail.com>
2026-02-05 19:20:10 +0530

    use the same model as for captain assistant

--- lib/captain/conversation_completion_service.rb
+++ lib/captain/conversation_completion_service.rb
@@ -16,7 +16,7 @@ class Captain::ConversationCompletionService < Captain::BaseTaskService
     return default_incomplete_response('No messages found') if content.blank?
 
     response = make_api_call(
-      model: GPT_MODEL,
+      model: InstallationConfig.find_by(name: 'CAPTAIN_OPEN_AI_MODEL')&.value.presence || GPT_MODEL,
       messages: [
         { role: 'system', content: prompt_from_file('conversation_completion') },
         { role: 'user', content: content }
